c4a2f04f7108c84dfa691476412e57af95096220
Nicholas Adamou <nicholasadamouemail@gmail.com>
2019-01-15 20:52:26 -0600

    Update main.sh

--- test/main.sh
+++ test/main.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd "$(dirname "${BASH_SOURCE[0]}")" \
-    && source <(curl -s "https://raw.githubusercontent.com/nicholasadamou/utilities/linux/utils.sh")
+    && source <(curl -s "https://raw.githubusercontent.com/nicholasadamou/utilities/utilities.sh")
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
